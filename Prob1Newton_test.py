2511f6341fd2432b51344831c5611d16bec32fd1
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-09-01 13:14:12 +0700

    Update Prob1Newton_test.py

--- Prob1Newton_test.py
+++ Prob1Newton_test.py
@@ -1,6 +1,17 @@
 import Prob1Newton;
 
-def test_newton():
+def test_newton1():
     eps =1e-4
-    ans = 0.0259355
+    ans = 0.347296354
     assert ans-eps <= BisectionInterest.BisectionInterest(0.1) <= ans+eps
+
+def test_newton2():
+    eps =1e-4
+    ans = -1.87938
+    assert ans-eps <= BisectionInterest.BisectionInterest(-1.1) <= ans+eps
+    
+    
+def test_newton3():
+    eps =1e-4
+    ans = 1.532088
+    assert ans-eps <= BisectionInterest.BisectionInterest(1.1) <= ans+eps
